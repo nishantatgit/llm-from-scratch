f614004ed0d48374afa0dd1ca21a859a5f03af41
nishantatgit <nishant.kumar.shreevastav@gmail.com>
2026-06-02 19:23:54 +0100

    implemented self attention

---
+++ src/SelfAttention.py
@@ -0,0 +1,21 @@
+import torch
+
+class SelfAttention(torch.nn.Module):
+    def __init__(self, in_dimension, out_dimension, qkv_bias=False):
+        self.W_Query = torch.nn.linear(in_dimension, out_dimension, bias=qkv_bias)
+        self.W_Key = torch.nn.linear(in_dimension, out_dimension, bias=qkv_bias)
+        self.W_Value = torch.nn.linear(in_dimension, out_dimension, bias=qkv_bias)
+
+    def forward(self,X):
+        queries = self.W_Query(X)
+        keys = self.W_Key(X)
+        values = self.W_Value(X)
+
+        attn_scores = queries @ keys.T
+        scaling_factor = keys.shape[-1] ** 0.5
+        attn_socres_scaled = attn_scores / scaling_factor
+
+        attn_weights = torch.softmax(attn_socres_scaled, dim = -1)
+        context_vector = attn_weights @ values
+
+        return context_vector
